2818b65fb854722213abf29f512e97b5a780f1a9
LyrisLaurier <1730336549@qq.com>
2023-07-12 11:02:54 +0800

    references update

--- test/panda_test.py
+++ test/panda_test.py
@@ -1,3 +1,66 @@
+"""
+https://www.runoob.com/pandas/pandas-tutorial.html
+
+读数据
+    pd.read_csv(filename)                   读取 CSV 文件；
+    pd.read_excel(filename)	                读取 Excel 文件；
+    pd.read_sql(query, connection_object)	从 SQL 数据库读取数据；
+    pd.read_json(json_string)	            从 JSON 字符串中读取数据；
+    pd.read_html(url)	                    从 HTML 页面中读取数据。
+
+查数据
+    df.head(n)	    显示前 n 行数据；
+    df.tail(n)	    显示后 n 行数据；
+    df.info()	    显示数据的信息，包括列名、数据类型、缺失值等；
+    df.describe()	显示数据的基本统计信息，包括均值、方差、最大值、最小值等；
+    df.shape	    显示数据的行数和列数。
+
+数据清洗
+    df.dropna()	                        删除包含缺失值的行或列；
+    df.fillna(value)	                将缺失值替换为指定的值；
+    df.replace(old_value, new_value)	将指定值替换为新值；
+    df.duplicated()	                    检查是否有重复的数据；
+    df.drop_duplicates()	            删除重复的数据。
+
+数据选择和切片
+    df[column_name]	                                选择指定的列；
+    df.loc[row_index, column_name]	                通过标签选择数据；
+    df.iloc[row_index, column_index]	            通过位置选择数据；
+    df.ix[row_index, column_name]	                通过标签或位置选择数据；
+    df.filter(items=[column_name1, column_name2])	选择指定的列；
+    df.filter(regex='regex')	                    选择列名匹配正则表达式的列；
+    df.sample(n)	                                随机选择 n 行数据。
+
+排序
+    df.sort_values(column_name)	                                            按照指定列的值排序；
+    df.sort_values([column_name1, column_name2], ascending=[True, False])	按照多个列的值排序；
+    df.sort_index()	                                                        按照索引排序。
+
+数据分组和聚合
+    df.groupby(column_name)	                        按照指定列进行分组；
+    df.aggregate(function_name)	                    对分组后的数据进行聚合操作；
+    df.pivot_table(values, index, columns, aggfunc)	生成透视表。
+
+数据合并
+    pd.concat([df1, df2])	            将多个数据框按照行或列进行合并；
+    pd.merge(df1, df2, on=column_name)	按照指定列将两个数据框进行合并。
+
+选择和过滤
+    df.loc[row_indexer, column_indexer]	    按标签选择行和列。
+    df.iloc[row_indexer, column_indexer]	按位置选择行和列。
+    df[df['column_name'] > value]	        选择列中满足条件的行。
+    df.query('column_name > value')	        使用字符串表达式选择列中满足条件的行。
+
+数据统计和描述
+    df.describe()	计算基本统计信息，如均值、标准差、最小值、最大值等。
+    df.mean()	    计算每列的平均值。
+    df.median()	    计算每列的中位数。
+    df.mode()	    计算每列的众数。
+    df.count()	    计算每列非缺失值的数量。
+
+"""
+
+
 import pandas as pd
 
 
@@ -73,14 +136,37 @@ def dataFrame_test():
 
 
 def csv_test():
-    df = pd.read_csv('nba.csv')
-    print(df.to_string())
+    # 三个字段 name, site, age
+    nm = ["Google", "Runoob", "Taobao", "Wiki"]
+    st = ["www.google.com", "www.runoob.com", "www.taobao.com", "www.wikipedia.org"]
+    ag = [90, 40, 80, 98]
+
+    # 字典
+    dict = {'name': nm, 'site': st, 'age': ag}
+
+    # 保存 dataframe
+    df = pd.DataFrame(dict)
+    df.to_csv('site.csv')
+
+    # print csv test
+    df = pd.read_csv('XSS_dataset.csv')
+    # print(df)
+    # print(df.to_string())
+    # to_string() 用于返回 DataFrame 类型的数据，
+    # 如果不使用该函数，则输出结果为数据的前面 5 行和末尾 5 行，中间部分以 ... 代替
+
+    # 读取前n行/后n行
+    # print(df.head(3))
+    # print(df.tail(3))
+
+    # 返回基本信息
+    print(df.info())
 
 
 if __name__ == '__main__':
-    # print('————series_test————')
-    # series_test()
-    # print('————dataFrame_test————')
-    # dataFrame_test()
+    print('————series_test————')
+    series_test()
+    print('————dataFrame_test————')
+    dataFrame_test()
     print('————csv_test————')
     csv_test()
