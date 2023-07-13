1d878adfe19aa51ee39573aafafa4e0a76243fab
uniqueFranky <y_r_b@qq.com>
2023-07-13 17:10:40 +0800

    fix debug

--- model.py
+++ model.py
@@ -59,7 +59,7 @@ def train(vocab_size, embedding_size, hidden_size, num_layers, output_size, num_
             optimizer.step()
         torch.save(model.state_dict(), 'checkpoint.ckp')
         print(f'checkpoint %d / %d written' %(epoch + 1, num_epoch))
-        for name, param in model.parameters():
+        for name, param in model.named_parameters():
             print(name, param)
             print(param.grad)
         with torch.no_grad():
